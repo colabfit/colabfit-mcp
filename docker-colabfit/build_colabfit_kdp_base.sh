87da144c5e40b033aca722d673cdddcd0da2d6ff
Navaneeth Mohan <mohan227@umn.edu>
2026-05-15 15:12:59 -0500

    basic instructions for starting container

--- docker-colabfit/build_colabfit_kdp_base.sh
+++ docker-colabfit/build_colabfit_kdp_base.sh
@@ -1,3 +1,5 @@
 docker build   -f /home/nmohan/UMN/colabfit-mcp/docker-colabfit/docker/minimal/Dockerfile   -t colabfit-minimal   /home/nmohan/UMN/colabfit-mcp;
 docker build   -f /home/nmohan/UMN/colabfit-mcp/docker-colabfit/docker/torchml/Dockerfile   -t colabfit-torchml   /home/nmohan/UMN/colabfit-mcp;
-#docker build -t colabfit-torchml docker/torchml;
+
+# start the container with port-forwarding
+# docker run -it -p 8000:8000 colabfit-minimal:latest bash
