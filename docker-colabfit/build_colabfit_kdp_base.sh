664a8dbfe85c9bf86508a9a9f244abd687139c34
Navaneeth Mohan <mohan227@umn.edu>
2026-05-29 11:34:54 -0500

    minor edit to build bashscript

--- docker-colabfit/build_colabfit_kdp_base.sh
+++ docker-colabfit/build_colabfit_kdp_base.sh
@@ -1,5 +1,11 @@
-docker build   -f /home/nmohan/UMN/colabfit-mcp/docker-colabfit/docker/minimal/Dockerfile   -t colabfit-minimal   /home/nmohan/UMN/colabfit-mcp;
-docker build   -f /home/nmohan/UMN/colabfit-mcp/docker-colabfit/docker/torchml/Dockerfile   -t colabfit-torchml   /home/nmohan/UMN/colabfit-mcp;
+# build minimal image (for search/download)
+	docker build   -f /home/nmohan/UMN/colabfit-mcp/docker-colabfit/docker/minimal/Dockerfile   -t colabfit-minimal   /home/nmohan/UMN/colabfit-mcp;
+
+# build torchml-image (for training)
+	# docker build   -f /home/nmohan/UMN/colabfit-mcp/docker-colabfit/docker/torchml/Dockerfile   -t colabfit-torchml   /home/nmohan/UMN/colabfit-mcp;
 
 # start the container with port-forwarding
-# docker run -it -p 8000:8000 colabfit-minimal:latest bash
+	# docker run -it -p 8000:8000 colabfit-minimal:latest bash
+
+# to start a singularity container - you dont need port-forwarding in singularity
+	# singularity exec colabfit-minimal.sif colabfit-mcp
